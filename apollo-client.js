8140a53d5175ad09128c2eaea71d263cfbee47a8
FlorianSahbi <florian.sahbi@gmail.com>
2021-04-10 16:07:37 +0200

    Add/New GraphQL endpoint

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  // uri: "http://localhost:4000/",
+  uri: "https://yu-server.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
