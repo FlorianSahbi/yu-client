9d1df288f2987f8b471e25f9c6a8ae5bfdf04981
FlorianSahbi <florian.sahbi@gmail.com>
2021-04-29 05:22:34 +0200

    Add/Env in cofig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  env: {
+    NEXT_PUBLIC_APOLLO_URI: process.env.NEXT_PUBLIC_APOLLO_URI,
+    NEXT_PUBLIC_CLIENT_ID: process.env.NEXT_PUBLIC_CLIENT_ID,
+  },
   reactStrictMode: false,
   future: {
     webpack5: true,
